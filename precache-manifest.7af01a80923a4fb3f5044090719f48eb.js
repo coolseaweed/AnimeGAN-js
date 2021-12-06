14d63bf7802fd17fdb95f3ce673fb59a208e264b
coolseaweed <jeellato@gmail.com>
2021-12-06 20:11:42 +0900

    Updates

---
+++ precache-manifest.7af01a80923a4fb3f5044090719f48eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1608d2522931c1003796b3acd3b0d9e",
+    "url": "/AnimeGAN-js/index.html"
+  },
+  {
+    "revision": "9645e3cc9733f2a16538",
+    "url": "/AnimeGAN-js/static/css/2.3e819d00.chunk.css"
+  },
+  {
+    "revision": "95b388990db5bfd62cd3",
+    "url": "/AnimeGAN-js/static/css/main.0fd4d4f4.chunk.css"
+  },
+  {
+    "revision": "9645e3cc9733f2a16538",
+    "url": "/AnimeGAN-js/static/js/2.191ee44d.chunk.js"
+  },
+  {
+    "revision": "8a816f10efd79d8c9a66df8a556eecef",
+    "url": "/AnimeGAN-js/static/js/2.191ee44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b388990db5bfd62cd3",
+    "url": "/AnimeGAN-js/static/js/main.6a287269.chunk.js"
+  },
+  {
+    "revision": "d6eed6f1160151e445bc",
+    "url": "/AnimeGAN-js/static/js/runtime-main.d8882604.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08f00fb5c56e2c48a40c67957bb13e5d.js"
+  "/AnimeGAN-js/precache-manifest.7af01a80923a4fb3f5044090719f48eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AnimeGAN-js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
